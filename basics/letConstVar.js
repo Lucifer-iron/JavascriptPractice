219f49075a98431bdd74314bad206bd4f073f9e9
Amit Hubraj Pal <87354086+Lucifer-iron@users.noreply.github.com>
2023-10-02 10:06:37 +0000

    ways  to do variable declaration in javascript. how to comment a code. scope of variable.

---
+++ basics/letConstVar.js
@@ -0,0 +1,16 @@
+let city = "mumbai";    // block level scoped variable declaration
+var state = "maharashtra";  // globally scoped variable declaration 
+const country = "india";     // to declare a variable whose value is constant i.e it doesn't changes.
+world = "earth";             // a way of variable declaration
+
+let place ;   // variable declaration without assigning any value to it.
+
+
+ console.table([city,state,country,world,place])  // to print variable on console in tabular form
+
+ //this is a single line comment 
+
+
+ /*
+this is a multi line comment
+ */
